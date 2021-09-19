fcdb2ff03ed25afae41c56a8acd2e4e6b31162ff
dylan edwards <dylanedwards@dylans-MacBook-Pro.local>
2021-09-19 11:16:26 -0700

    init hw 2

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# You will need to build two models for this homework
+# - one using Naive Bayes and one using Logistic Regression.
+# Both of your models should do the same task of classifying reviews into either “positive” or “negative” based on chosen features.
+# Then based on the definition of “fake”, you can do additional analysis to flag any reviews that could be “fake”.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
